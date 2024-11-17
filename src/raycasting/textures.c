f6097d47f8613bf2e2140dd18610bd5bf91aa2a4
ftTower <thomas.auer67@gmail.com>
2024-11-17 22:54:11 +0100

    refactor: remove debug print statements and simplify texture drawing logic

--- src/raycasting/textures.c
+++ src/raycasting/textures.c
@@ -16,7 +16,6 @@ int	get_color_texture(t_file *texture, int x, int y)
 	return (color);
 }
 t_file *get_texture(t_data *data, t_dir dir) {
-    // printf("Direction: %d\n", dir); // Ajoutez ceci pour déboguer
     if (dir == DIR_EAST)
         return data->config->EA;
     else if (dir == DIR_WEAST)
@@ -36,43 +35,22 @@ void vertical_draw_texture(t_data *data, t_img *img, t_ray *r_c)
     t_file *texture;
 
     texture = get_texture(data, r_c->direction);
-
     wall_height = r_c->end - r_c->start;
-
-    // printf("Direction: %d\n", r_c->direction);
-    // printf("Wall height: %f\n", wall_height);
-    // printf("Start: %f, End: %f\n", r_c->start, r_c->end);
-
     if (r_c->direction == DIR_NORTH || r_c->direction == DIR_SOUTH)
-    {
         tex_x = (int)((r_c->end_x - (int)r_c->end_x) * texture->img->width);
-    }
     else
-    {
         tex_x = (int)((r_c->end_y - (int)r_c->end_y) * texture->img->width);
-    }
-
-    // tex_x = tex_x < 0 ? 0 : tex_x;
-    // tex_x = tex_x >= texture->img->width ? texture->img->width - 1 : tex_x;
-
     index = r_c->start;
     while (index < r_c->end)
     {
-
         tex_y = (int)(((index - r_c->start) / wall_height) * texture->img->height);
-
-
         tex_y = tex_y < 0 ? 0 : tex_y;
         tex_y = tex_y >= texture->img->height ? texture->img->height - 1 : tex_y;
-
-
         my_mlx_pixel_put(img, r_c->w_line, (int)index,
             get_color_texture(texture, tex_x, tex_y));
-
         index++;
     }
 }
-
 
 void	vertical_draw(t_data *data, t_img *img, t_ray *r_c, t_draw type_code)
 {
